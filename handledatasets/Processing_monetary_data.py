3e7796ec88bffd7d86a8c9b3a086ac1d2557cdbc
tantd9 <tranduytan.220300@gmail.com>
2024-03-31 18:44:58 +0700

    update dataset

--- handledatasets/Processing_monetary_data.py
+++ handledatasets/Processing_monetary_data.py
@@ -2,24 +2,27 @@ import pandas as pd
 import re
 
 def remove_special_chars(input_string):
-    # Regex pattern để loại bỏ tất cả các ký tự đặc biệt và số, bao gồm cả "vietkey"
-    pattern = r'[^a-zA-Z\sàáảãạăắằẳẵặâấầẩẫậèéẻẽẹêếềểễệìíỉĩịòóỏõọôốồổỗộơớờởỡợùúủũụưứừửữựỳýỷỹỵđ]'
-    return re.sub(pattern, '', input_string)
+    if isinstance(input_string, str):  # Check if input_string is a string
+        # Regex pattern to remove all special characters and numbers
+        pattern = r'[^a-zA-Z\sàáảãạăắằẳẵặâấầẩẫậèéẻẽẹêếềểễệìíỉĩịòóỏõọôốồổỗộơớờởỡợùúủũụưứừửữựỳýỷỹỵđ]'
+        return re.sub(pattern, '', input_string)
+    else:
+        return str(input_string)  # Convert non-string input to string
 
 def process_excel(input_file, output_file):
-    # Đọc dữ liệu từ file Excel vào DataFrame
+    # Read data from Excel file into DataFrame
     df = pd.read_excel(input_file)
     
-    # Áp dụng hàm remove_special_chars cho cột "Sentence"
+    # Apply remove_special_chars function to the "Sentence" column
     df['Processed_Sentence'] = df['Sentence'].apply(remove_special_chars)
     
-      # Loại bỏ các dòng trùng nhau
+    # Remove duplicate rows
     df = df.drop_duplicates()
     
-    # Ghi dữ liệu đã xử lý ra file Excel
+    # Write processed data to Excel file
     df.to_excel(output_file, index=False)
 
-# Sử dụng hàm:
+# Usage:
 input_file = "data/data.xlsx"
 output_file = "data/output.xlsx"
 process_excel(input_file, output_file)
